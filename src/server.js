a9dee73108814fe12b2ba4426404f368ad0da853
MinChan Lee <shigatsu970704@gmail.com>
2021-01-04 17:49:02 +0900

    05.SocektIO Connection Part One

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { join } from "path";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => res.render("home"));
@@ -17,3 +19,11 @@ const handleListening = () =>
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
+
+setInterval(() => console.log(sockets), 1000);
